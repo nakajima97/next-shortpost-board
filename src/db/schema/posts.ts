85fef5603f9d24531dfd42d641d74748270a22ec
nakajima97 <kituneudon97@gmail.com>
2025-09-23 16:46:52 +0900

    #7 biomeの実行

--- src/db/schema/posts.ts
+++ src/db/schema/posts.ts
@@ -1,15 +1,11 @@
-import { sql } from "drizzle-orm";
-import { int, text } from "drizzle-orm/sqlite-core";
-import { sqliteTable } from "drizzle-orm/sqlite-core";
+import { sql } from 'drizzle-orm';
+import { int, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable } from 'drizzle-orm/sqlite-core';
 
 export const posts = sqliteTable('posts', {
   id: int('id').primaryKey(),
   handleName: text('handle_name').notNull(),
   content: text('content').notNull(),
-  createdAt: text("created_at")
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: text("updated_at")
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-})
+  createdAt: text('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+});
