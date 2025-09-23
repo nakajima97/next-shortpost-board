dd1ba4679a0c116754a438c224823ce72c09e10b
nakajima97 <kituneudon97@gmail.com>
2025-09-23 12:33:59 +0900

    #4 shadcnの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/shadcn/button";
+
 export default function Home() {
   return (
     <main>
       <p>Hello, world!</p>
       <p className="text-4xl font-bold underline">Welcome to Next.js!</p>
+      <Button>test</Button>
     </main>
   );
 }
